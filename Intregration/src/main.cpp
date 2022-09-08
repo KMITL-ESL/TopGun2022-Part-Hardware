f75a193a9a0df8c17214df092bdcc046b8d1a20f
Witchayut Gerdchai <gmplk@hotmail.com>
2022-09-09 00:41:09 +0700

    maybe read gyro

--- Intregration/src/main.cpp
+++ Intregration/src/main.cpp
@@ -6,6 +6,7 @@
 #include <PubSubClient.h>
 #include <Adafruit_ADS1X15.h>
 #include <Arduino_JSON.h>
+#include "Madgwick_Quaternion.h"
 
 // include Font
 #include "quark24.h"
@@ -30,14 +31,17 @@ const char* topic = "KMITL-02/";
 WiFiClient client;
 PubSubClient mqtt(client);
 Adafruit_ADS1115 ads;
+Madgwick_Quaternion M_Q_Filter;
 
 // millis
 long long lastSend;
-long long sendDelay=1000; // 100 ms
+long long sendDelay=500; // 100 ms
 long long lastMqttReconnect;
 long long mqttReconnectDelay=5000; // 5 s
 long long lastReadPotentiometer;
 long long readPotentiometerDelay=100; // 100 ms
+long long lastReadIMU;
+long long readIMUDelay=10; // 10 ms
 
 // calibrate potentiometer
 int finger0min = 27000;
@@ -54,10 +58,29 @@ int finger0 = 0;
 int finger1 = 0;
 int finger2 = 0;
 int finger3 = 0;
+float pitch = 0.0F;
+float roll  = 0.0F;
+float yaw   = 0.0F;
+float quat_w = 0.0F;
+float quat_x = 0.0F;
+float quat_y = 0.0F;
+float quat_z = 0.0F;
 
-float gyroX = 0.0F;
-float gyroY = 0.0F;
-float gyroZ = 0.0F;
+// offset sensor imu
+float acc[3];
+float accOffset[3];
+float gyro[3];
+float gyroOffset[3];
+float mag[3];
+float magOffset[3];
+float magmax[3];
+float magmin[3];
+uint8_t setup_flag = 1;
+uint8_t action_flag = 1;
+float heading = 0;
+
+uint8_t smoothen = 100;
+float strength = 1;
 
 
 // funtion prototype
@@ -71,9 +94,20 @@ void readPotentiometer();
 void updateReadPotentiometer();
 void sendMQTTfinger();
 void updateSendMQTTfinger();
+void sendMQTTGyro();
+void calibrate6886();
+void calibrate_waiting(uint32_t timeout);
+void applycalibration();
+void IMU_Update();
+void updateSendMQTTGyro();
+void updateReadIMU();
+void updateReCalibrate();
 
 void setup() {
+
+  // init M5StickCPlus
   M5.begin();
+  M5.Imu.Init();   
   
   // display startup
   M5.Lcd.fillScreen(BLACK);
@@ -84,28 +118,37 @@ void setup() {
   M5.Lcd.println("Connecting to " + String(ssid) + "...");
 
   // connect to wifi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    M5.Lcd.print(".");
-  }
+  // WiFi.begin(ssid, password);
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(500);
+  //   M5.Lcd.print(".");
+  // }
 
   // display wifi connected
-  M5.Lcd.println("Connected");
-  M5.Lcd.println("IP address: " + WiFi.localIP().toString());
+  // M5.Lcd.println("Connected");
+  // M5.Lcd.println("IP address: " + WiFi.localIP().toString());
 
   // connect to mqtt
-  mqttSetup();
+  // mqttSetup();
 
   // display mqtt connected
-  M5.Lcd.println("MQTT connected");
-  M5.Lcd.println("Chip ID: " + String(chipId));
+  // M5.Lcd.println("MQTT connected");
+  // M5.Lcd.println("Chip ID: " + String(chipId));
 
   // start ads1115
   ads.begin();
 
   // display ads1115 connected
   M5.Lcd.println("ADS1115 connected");
+
+  // calibrate imu
+  calibrate6886();
+
+  // begin imu
+  M_Q_Filter.begin(smoothen); //100Hz
+
+  // calibrate imu again
+  calibrate_waiting(10);
 
   // display ready
   M5.Lcd.println("Ready");
@@ -116,10 +159,13 @@ void loop() {
 }
 
 void update(){
-  // M5.update();
-  updateMQTT();
+  M5.update();
+  // updateMQTT();
   // updateReadPotentiometer();
-  updateSendMQTTfinger();
+  // updateSendMQTTfinger();
+  updateReCalibrate();
+  updateSendMQTTGyro();
+  updateReadIMU();
 }
 
 void getChipID(){
@@ -246,18 +292,222 @@ void updateSendMQTTGyro(){
 }
 
 void sendMQTTGyro(){
-  // get gyro data
-  M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
   
-  // make json string
-  JSONVar packageJSON;
-  packageJSON["x"] = gyroX;
-  packageJSON["y"] = gyroY;
-  packageJSON["z"] = gyroZ;
-  String packageString = JSON.stringify(packageJSON);
-  Serial.println(packageString);
-  mqtt.publish("KMITL-02/gyro", packageString.c_str());
-  Serial.println("Send gyro");
+  // print value
+  // Serial.print("quat_x: ");
+  // Serial.print(quat_x);
+  // Serial.print("\tquat_y: ");
+  // Serial.print(quat_y);
+  // Serial.print("\tquat_z: ");
+  // Serial.println(quat_z);
+  
+  // // make json string
+  // JSONVar packageJSON;
+  // packageJSON["x"] = gyroX;
+  // packageJSON["y"] = gyroY;
+  // packageJSON["z"] = gyroZ;
+  // String packageString = JSON.stringify(packageJSON);
+  // Serial.println(packageString);
+  // mqtt.publish("KMITL-02/gyro", packageString.c_str());
+  // Serial.println("Send gyro");
+}
+
+void calibrate6886(){
+  double gyroSum[3];
+  double accSum[3];
+  int counter = 5;
+
+  Serial.println("Calibrating...");
+
+  // for (int i = 0; i < counter; i++) {
+  //   M5.Imu.getGyroData(&gyro[0], &gyro[1], &gyro[2]);
+  //   M5.Imu.getAccelData(&acc[0], &acc[1], &acc[2]);
+
+  //   // Serial.print("gyro[0]: ");
+  //   // Serial.print(gyro[0]);
+  //   // Serial.print(" | gyro[1]: ");
+  //   // Serial.print(gyro[1]);
+  //   // Serial.print(" | gyro[2]: ");
+  //   // Serial.println(gyro[2]);
+  //   // Serial.print("acc[0]: ");
+  //   // Serial.print(acc[0]);
+  //   // Serial.print(" | acc[1]: ");
+  //   // Serial.print(acc[1]);
+  //   // Serial.print(" | acc[2]: ");
+  //   // Serial.println(acc[2]);
+
+  //   gyroSum[0] += gyro[0];
+  //   gyroSum[1] += gyro[1];
+  //   gyroSum[2] += gyro[2];
+  //   accSum[0] += acc[0];
+  //   accSum[1] += acc[1];
+  //   accSum[2] += acc[2];
+
+  //   // clear value
+  //   gyro[0] = 0;
+  //   gyro[1] = 0;
+  //   gyro[2] = 0;
+  //   acc[0] = 0;
+  //   acc[1] = 0;
+  //   acc[2] = 0;
+
+  //   delay(2);
+  // }
+
+  M5.Imu.getGyroData(&gyro[0], &gyro[1], &gyro[2]);
+  M5.Imu.getAccelData(&acc[0], &acc[1], &acc[2]);
+
+  gyroOffset[0] = gyro[0];
+  gyroOffset[1] = gyro[1];
+  gyroOffset[2] = gyro[2];
+  accOffset[0] = acc[0];
+  accOffset[1] = acc[1];
+  accOffset[2] = acc[2] - 1.0;  //Gravitational Acceleration 1G, assuming that the M5 button is facing upward
+
+  Serial.println("Calibration Done!");
+  Serial.print("gyroOffset[0]: ");
+  Serial.print(gyroOffset[0]);
+  Serial.print(" | gyroOffset[1]: ");
+  Serial.print(gyroOffset[1]);
+  Serial.print(" | gyroOffset[2]: ");
+  Serial.println(gyroOffset[2]);
+  Serial.print("accOffset[0]: ");
+  Serial.print(accOffset[0]);
+  Serial.print(" | accOffset[1]: ");
+  Serial.print(accOffset[1]);
+  Serial.print(" | accOffset[2]: ");
+  Serial.println(accOffset[2]);
+
+}
+
+void calibrate_waiting(uint32_t timeout){
+  int16_t value_x_min = 0;
+  int16_t value_x_max = 0;
+  int16_t value_y_min = 0;
+  int16_t value_y_max = 0;
+  int16_t value_z_min = 0;
+  int16_t value_z_max = 0;
+  uint32_t timeStart = 0;
+
+  timeStart = millis();
+
+  while ((millis() - timeStart) < timeout)
+  {
+    if (digitalRead(M5_BUTTON_HOME) == LOW) {
+      setup_flag = 1;
+      while (digitalRead(M5_BUTTON_HOME) == LOW);
+      break;
+    }
+    delay(100);
+  }
+
+}
+
+void applycalibration(){
+  M5.Imu.getGyroData(&gyro[0], &gyro[1], &gyro[2]);
+  M5.Imu.getAccelData(&acc[0], &acc[1], &acc[2]);
+
+  // print raw value
+  // Serial.println("Raw Value");
+  // Serial.print("gyro[0]: ");
+  // Serial.print(gyro[0]);
+  // Serial.print(" | gyro[1]: ");
+  // Serial.print(gyro[1]);
+  // Serial.print(" | gyro[2]: ");
+  // Serial.println(gyro[2]);
+  // Serial.print("acc[0]: ");
+  // Serial.print(acc[0]);
+  // Serial.print(" | acc[1]: ");
+  // Serial.print(acc[1]);
+  // Serial.print(" | acc[2]: ");
+  // Serial.println(acc[2]);
+
+
+  gyro[0] -= gyroOffset[0];
+  gyro[1] -= gyroOffset[1];
+  gyro[2] -= gyroOffset[2];
+  acc[0] -= accOffset[0];
+  acc[1] -= accOffset[1];
+  acc[2] -= accOffset[2];
+
+  // print offset value
+  // Serial.println("Offset Value");
+  // Serial.print("gyroOffset[0]: ");
+  // Serial.print(gyroOffset[0]);
+  // Serial.print(" | gyroOffset[1]: ");
+  // Serial.print(gyroOffset[1]);
+  // Serial.print(" | gyroOffset[2]: ");
+  // Serial.println(gyroOffset[2]);
+  // Serial.print("accOffset[0]: ");
+  // Serial.print(accOffset[0]);
+  // Serial.print(" | accOffset[1]: ");
+  // Serial.print(accOffset[1]);
+  // Serial.print(" | accOffset[2]: ");
+  // Serial.println(accOffset[2]);
+
+  // Serial.println("Calibrated Value");
+  // Serial.print("gyro[0]: ");
+  // Serial.print(gyro[0]);
+  // Serial.print(" | gyro[1]: ");
+  // Serial.print(gyro[1]);
+  // Serial.print(" | gyro[2]: ");
+  // Serial.println(gyro[2]);
+  // Serial.print("acc[0]: ");
+  // Serial.print(acc[0]);
+  // Serial.print(" | acc[1]: ");
+  // Serial.print(acc[1]);
+  // Serial.print(" | acc[2]: ");
+  // Serial.println(acc[2]);
+
+  //fake magnetometer data cuz MPU6886 doesn't come with BMM 150 chip
+  mag[0] = 0;
+  mag[1] = 0;
+  mag[2] = 0;
+}
+
+void IMU_Update() {
+  applycalibration();
+
+  heading = atan2(mag[0], mag[1]);
+  if (heading < 0)
+    heading += 2 * PI;
+  if (heading > 2 * PI)
+    heading -= 2 * PI;
+
+  M_Q_Filter.update(gyro[0]*strength, gyro[1]*strength, gyro[2]*strength, acc[0], acc[1], acc[2], mag[0], mag[1], mag[2]);
+
+  roll = M_Q_Filter.getRoll();
+  pitch = M_Q_Filter.getPitch();
+  yaw   = M_Q_Filter.getYaw();
+
+  //for quaternion output (by zeketan)
+  quat_w = M_Q_Filter.getQuat_W();
+  quat_x = M_Q_Filter.getQuat_X();
+  quat_y = M_Q_Filter.getQuat_Y();
+  quat_z = M_Q_Filter.getQuat_Z(); //-0.005 anti yaw drifting
+
+  Serial.print("quat_x: ");
+  Serial.print(quat_x);
+  Serial.print("\tquat_y: ");
+  Serial.print(quat_y);
+  Serial.print("\tquat_z: ");
+  Serial.println(quat_z);
+}
+
+void updateReadIMU(){
+  if ((millis() - lastReadIMU) >= readIMUDelay) {
+    lastReadIMU = millis();
+    IMU_Update();
+  }
+}
+
+void updateReCalibrate(){
+  // Button A
+  if (M5.BtnA.wasReleased()) {
+    // reset esp32
+    Serial.println("Reset ESP32");
+    ESP.restart();
+  }
 }
 
 
